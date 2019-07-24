05e562d4a82cd3c695df8d5476017d68f0cd5784
Erix <xubaizhou@qq.com>
2019-07-24 10:19:23 -0700

    add some derivatives

--- MNIST_from_scratch/source/activation.py
+++ MNIST_from_scratch/source/activation.py
@@ -1,10 +1,25 @@
-import math
+from math import exp
+import numpy as np
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x*0.1))
+    return 1 / (1 + exp(-x*1.0))
 
 def relu(x):
-    return x if x >=0 else 0
+    return x if x >=0 else 0.0
 
 def tanh(x):
-    return 1.0 - 2/(1+math.exp(x+x))
+    return 1.0 - 2/(1+exp(x+x))
+
+def relu6(x):
+    return min(max(0,x),6)
+
+def softmax(x):
+    return exp(x) / exp(x).sum()
+
+sigmoid = np.vectorize(sigmoid)
+relu = np.vectorize(relu)
+tanh = np.vectorize(tanh)
+relu6 = np.vectorize(relu6)
+exp = np.vectorize(exp)
+
+
